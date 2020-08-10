96d0feb86f78efa5ae6a3698aeeb15a780c6a00b
Simon Tharby <jinjamail@gmail.com>
2020-08-10 18:02:38 +0100

    tutorial setup

--- src/tutorial.js
+++ src/tutorial.js
@@ -1,3 +1,28 @@
-let testText = document.getElementById('test-text');
+import * as THREE from '../js/three.module.js';
 
-testText.innerHTML = "hello from tutorial.js";
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// game logic
+var update = function() {
+
+};
+
+// draw scene
+var render = function() {
+  renderer.render(scene, camera);
+};
+
+// run game loop (update, render, repeat)
+var Gameloop = function() {
+  requestAnimationFrame(Gameloop);
+
+  update();
+  render();
+};
+
+Gameloop();
