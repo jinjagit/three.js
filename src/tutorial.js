6f39b61780b227e736756b98ba47a7257159f60b
Simon Tharby <jinjamail@gmail.com>
2020-08-10 21:48:13 +0100

    create info func for logging info to browser console

--- src/tutorial.js
+++ src/tutorial.js
@@ -1,5 +1,12 @@
 import * as THREE from '../js/three.module.js';
 
+const info = () => {
+  console.log("vertices:");
+  console.log(geometry.vertices);
+  console.log("faces:");
+  console.log(geometry.faces);
+};
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -7,13 +14,12 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// create the shape
 // Isocahedron: (radius, detail) detail 0 - 5, 6 iterations or more is a lot of vertices!
-var geometry = new THREE.IcosahedronGeometry(1, 3);
+var geometry = new THREE.IcosahedronGeometry(1, 0);
 
 //geometry.vertices[0] = new THREE.Vector3(-1.2, 1.5, 0);
 
-console.log(geometry.vertices);
+info();
 
 // create a material, color, or image texture
 var material = new THREE.MeshNormalMaterial({color: 0xFFFFFF, wireframe: true});
