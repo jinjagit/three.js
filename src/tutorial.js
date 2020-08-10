fd6cecbc5a3e268ce0f749ef1d4fbd8b33856bfa
Simon Tharby <jinjamail@gmail.com>
2020-08-10 20:35:29 +0100

    tutorial pt. 2: draw geometry

--- src/tutorial.js
+++ src/tutorial.js
@@ -7,9 +7,20 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// create the shape
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+
+// create a material, color, or image texture
+var material = new THREE.MeshBasicMaterial({color: 0xFFFFFF, wireframe: false});
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 3;
+
 // game logic
 var update = function() {
-
+  cube.rotation.x += 0.0025;
+  cube.rotation.y += 0.005;
 };
 
 // draw scene
